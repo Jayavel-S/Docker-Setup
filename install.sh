f3005bbcee57b8f5bdab01180dfe9858af7a686a
Jayavel S <101336634+Jayavel-S@users.noreply.github.com>
2022-03-11 22:44:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ wget -qO- https://get.docker.com/ | sh
 sudo usermod -aG docker ${USER}
 
 # Install the prerequisites for setting up docker-compose
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
+sudo apt-get install -y libffi-dev libssl-dev
+sudo apt-get install -y python3-dev
 sudo apt-get install -y python3 python3-pip
 
 # Installing docker-compose using pip3
-sudo pip3 install docker-compose
+sudo pip3 install -y docker-compose
 
 # Enabling docker to start automatically on hardware reboot
 sudo systemctl enable docker
