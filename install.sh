9c890a3a4e7751c3be08f3b588eab0f555801151
Jayavel S <101336634+Jayavel-S@users.noreply.github.com>
2022-03-11 22:34:58 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+# Ask for the user password
+# Caching sudo access for install completion
+sudo true
+
+# Official docker install script
+wget -qO- https://get.docker.com/ | sh
+
+# Adding User Permissions
+sudo usermod -aG docker ${USER}
+
+# Install the prerequisites for setting up docker-compose
+sudo apt-get install libffi-dev libssl-dev
+sudo apt install python3-dev
+sudo apt-get install -y python3 python3-pip
+
+# Installing docker-compose using pip3
+sudo pip3 install docker-compose
+
+# Enabling docker to start automatically on hardware reboot
+sudo systemctl enable docker
